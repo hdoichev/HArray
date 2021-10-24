17154646477d283399d0ae083324e166ef4c7f24
Hristo Doichev <hristod@mac.com>
2021-10-24 00:32:35 -0500

    Cleaned up Array extension

--- Sources/HArray/Extensions.swift
+++ Sources/HArray/Extensions.swift
@@ -6,23 +6,19 @@
 //
 
 import Foundation
-
-extension Array: Storable, StorableAllocator where Array.Element: Codable {
-    
+///
+extension Array: StorableAllocator where Array.Element: Codable {
+    public typealias Storage = Array
     public func createStore(capacity: Int) -> Array<Element> {
         return Array()
     }
-    
-    public typealias Storage = Array
+}
+///
+extension Array: Storable where Array.Element: Codable {
     public mutating func replace(with elements: [Element]) {
         self = elements
     }
     public mutating func append(_ elements: [Element]) {
         self += elements
-    }
-    public mutating func split(at position: Int) -> Array<Element> {
-        let r = Array(self[position..<self.count])
-        self = Array(self[0..<position])
-        return r
     }
 }
